d60ef4d17c48d80c8b11f0194ee385bd0f6ce910
EC2 Default User <ec2-user@ip-172-31-27-11.us-west-2.compute.internal>
2022-08-07 01:37:17 +0000

    Sam code

--- Lambda/main.py
+++ Lambda/main.py
@@ -1,50 +1,73 @@
+import json
+import urllib.parse
+import boto3
+
+def lambda_handler(event, context):
+    """Sample pure Lambda function
+    Parameters
+    ----------
+    event: dict, required
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+    
     #print(e)
-    print(‘STEP 1 - Entering Lambda Handlder!’)
-    s3 = boto3.client(‘s3’)
-    print(‘STEP 2 - Calling S3 bucket!’)
-    bucket = event[‘Records’][0][‘s3’][‘bucket’][‘name’]
+    print("NIKHIL")
+    print('STEP 1 - Entering Lambda Handlder!')
+    objectS3 = boto3.client('s3')
+    
+    print('STEP 2 - Calling S3 bucket!')
+    bucket = event['Records'][0]['s3']['bucket']['name']
     print(bucket)
-    print(‘STEP 3 - Fatching the Key!’)
-    key = urllib.parse.unquote_plus(event[‘Records’][0][‘s3’][‘object’][‘key’], encoding=‘utf-8’)
+    
+    print('STEP 3 - Fatching the Key!')
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     print(key)
-    print(‘STEP 4 - Fatching the S3 Object’)
+    
+    print('STEP 4 - Fatching the S3 Object')
     try:
         response = s3.get_object(Bucket=bucket, Key=key)
-        print(“CONTENT TYPE: ” + response[‘ContentType’])
-        print(‘STEP 5 - Completed S3 bucket operation!’)
+        print('CONTENT TYPE: ' + response['ContentType'])
+        print('STEP 5 - Completed S3 bucket operation!')
         #return response[‘ContentType’]
-        client1 = boto3.client(‘textract’)
-        print(‘STEP 6 - Configured TextRact!’)
-        response1 = client1.analyze_id(DocumentPages=[{‘S3Object’: {‘Bucket’: bucket, ‘Name’: key}}])
-        print(‘STEP 7 - Got response from TextRact!’)
-        print(‘STEP 8 - Writing to DynamoDB!’)
-        client2 = boto3.client(‘dynamodb’)
-        data2 = client2.put_item(TableName=‘licenseid’, Item={
-            ‘ID’: {
+        
+        client1 = boto3.client('textract')
+        print('STEP 6 - Configured TextRact!')
+        
+        
+        response1 = client1.analyze_id(DocumentPages=[{'S3Object': {'Bucket': bucket, 'Name': key}}])
+        print('STEP 7 - Got response from TextRact!')
+        print('STEP 8 - Writing to DynamoDB!')
+        client2 = boto3.client('dynamodb')
+        data2 = client2.put_item(TableName='licenseid', Item={
+            'ID': {
             #‘S’: ‘JASON’
-            ‘S’: response1.IdentityDocuments[0].IdentityDocumentFields[0].ValueDetection.Text
+            'S': response1.IdentityDocuments[0].IdentityDocumentFields[0].ValueDetection.Text
             }}
         )
-        print(‘STEP 9 - Sending SNS Notification!’)
-        notification = “Here is the SNS notification for Lambda function tutorial.”
-        client5 = boto3.client(‘sns’)
+        print('STEP 9 - Sending SNS Notification!')
+        notification = 'Here is the SNS notification for Lambda function tutorial.'
+        client5 = boto3.client('sns')
         response5 = client5.publish (
-            TargetArn = “arn:aws:sns:us-west-1:468885651811:licensetopic”,
-           Message = json.dumps({‘default’: notification}),
-            MessageStructure = ‘json’
+            TargetArn = 'arn:aws:sns:us-west-1:468885651811:licensetopic',
+           Message = json.dumps({'default': notification}),
+            MessageStructure = 'json'
         )
         print(response1)
     except Exception as e:
         print(e)
-        print(‘Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.’.format(key, bucket))
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
         raise e
-    print(‘About to return1!’)
+    print('About to return1!')
     return {
-        “statusCode”: 200,
-        “body”: json.dumps({
-            “message”: “Image added to DynamoDB and SNS notificastion sent”,
+        'statusCode': 200,
+        'body': json.dumps({
+            'message': 'Image added to DynamoDB and SNS notificastion sent',
             # “location”: ip.text.replace(“\n”, “”)
         }),
+        
     }
     
-    */
+a=''
+b=2
+lambda_handler(a, b)
---
+++ dynamodb_event_reader/README.md
@@ -0,0 +1,93 @@
+# dynamodb_event_reader
+
+Sample SAM Template for dynamodb_event_reader to interact with DynamoDB Events.
+
+## Requirements
+
+* AWS CLI already configured with at least PowerUser permission
+
+* [Python 3 installed](https://www.python.org/downloads/)
+
+* [Docker installed](https://www.docker.com/community-edition)
+* [SAM Local installed](https://github.com/awslabs/aws-sam-local)
+
+
+### Build Your Application
+
+When you make a change to the code, you can run the following command to install dependencies
+and convert your Lambda function source code into an artifact that can be deployed and run on Lambda.
+
+```bash
+sam build
+```
+
+### Local development
+
+Assuming you have already built your application, run the following to invoke your function locally:
+
+**Invoking function locally without API Gateway**
+
+```bash
+sam local generate-event dynamodb update | sam local invoke ReadDynamoDBEvent
+```
+
+You can also specify a `event.json` file with the payload you'd like to invoke your function with:
+
+```bash
+sam local invoke -e event.json ReadDynamoDBEvent
+```
+
+## Deployment
+
+First and foremost, we need a S3 bucket where we can upload our Lambda functions packaged as ZIP before we deploy anything - If you don't have a S3 bucket to store code artifacts then this is a good time to create one:
+
+```bash
+aws s3 mb s3://BUCKET_NAME
+```
+
+Now build your Lambda function
+
+```bash
+sam build
+```
+
+Provided you have a S3 bucket created, run the following command to package our Lambda function to S3:
+
+```bash
+sam package \
+    --output-template-file packaged.yaml \
+    --s3-bucket REPLACE_THIS_WITH_YOUR_S3_BUCKET_NAME
+```
+
+Next, the following command will create a Cloudformation Stack and deploy your SAM resources.
+
+```bash
+sam deploy \
+    --template-file packaged.yaml \
+    --stack-name dynamodb_event_reader \
+    --capabilities CAPABILITY_IAM
+```
+
+> **See [Serverless Application Model (SAM) HOWTO Guide](https://github.com/awslabs/serverless-application-model/blob/master/HOWTO.md) for more details in how to get started.**
+
+# Appendix
+
+## AWS CLI commands
+
+AWS CLI commands to package, deploy and describe outputs defined within the cloudformation stack:
+
+```bash
+sam package \
+    --template-file template.yaml \
+    --output-template-file packaged.yaml \
+    --s3-bucket REPLACE_THIS_WITH_YOUR_S3_BUCKET_NAME
+
+sam deploy \
+    --template-file packaged.yaml \
+    --stack-name dynamodb_event_reader \
+    --capabilities CAPABILITY_IAM \
+    --parameter-overrides MyParameterSample=MySampleValue
+
+aws cloudformation describe-stacks \
+    --stack-name dynamodb_event_reader --query 'Stacks[].Outputs'
+```
